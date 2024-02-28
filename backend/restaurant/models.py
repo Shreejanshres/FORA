bea005091608b06a3cc3f6d9830444fb62820ba4
Shreejanshres <87512980+Shreejanshres@users.noreply.github.com>
2024-02-28 20:52:02 +0545

    changed models of the Menu and Tag

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -18,17 +18,23 @@ class RestaurantData(models.Model):
     
 class Tags(models.Model):
     tag=models.CharField(max_length=50)
-    restaurant=models.ForeignKey(RestaurantData,on_delete=models.CASCADE,related_name="tags")
     
     def __str__(self):
         return self.tag
+
+class RestaurantTag(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.restaurant.restaurant_name} - {self.tag.tag_name}"
     
 class Menu(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField()
     price=models.DecimalField(max_digits=6,decimal_places=2)
-    picture=models.ImageField(upload_to='menu/')
     restaurant=models.ForeignKey(RestaurantData,on_delete=models.CASCADE,related_name="menu_items")
+    restaurant_tags = models.ManyToManyField(RestaurantTag) 
     tags=models.ManyToManyField(Tags,related_name="menu_items",blank=True)
 
     def __str__(self):
